502fc79fb936223a5e56ccb0292a7c7b4649330b
JoaoPiaui <joaovdf17@gmail.com>
2019-08-02 09:52:37 -0300

    Correção de erros

--- Exemplo04.js
+++ Exemplo04.js
@@ -1,17 +1,27 @@
 tarefas = [];
+var indiceParaEditar = -1; // criada fora de qualquer método se torna uma variável global.
 
-function salvar(e){
+function salvar(e) {
     // 13 é o código do enter no teclado
-    if(e.keyCode == 13){
+    if (e.keyCode == 13) {
+        adicionarEditar();
+    }
+}
+
+function adicionarEditar() {
+    if (indiceParaEditar == -1) {
         adicionar();
+    }
+    else {
+        editar();
     }
 }
 
 function adicionar() {
     var campoNome = document.getElementById("nome")
     var nome = campoNome.value;
-    valido = validar(nome,campoNome);
-    if(valido == false){
+    valido = validar(nome, campoNome);
+    if (valido == false) {
         //mostrar feedback
         return;
     }
@@ -24,15 +34,19 @@ function adicionar() {
     var elementoBotaoEditar = document.createElement("button");
     elementoBotaoEditar.innerHTML = "Editar";
     elementoBotaoEditar.classList.add("btn", "btn-primary", "mr-2");
+    elementoBotaoEditar.onclick = preencherCampo;
 
     var elementoBotaoApagar = document.createElement("button");
     elementoBotaoApagar.innerHTML = "Apagar";
     elementoBotaoApagar.classList.add("btn", "btn-danger");
+    elementoBotaoApagar.onclick = apagar;
+
+
 
     elementoTdAcao.appendChild(elementoBotaoEditar);
     elementoTdAcao.appendChild(elementoBotaoApagar);
 
-    
+
     elementotr.appendChild(elementoTdNome);
     elementotr.appendChild(elementoTdAcao);
 
@@ -42,19 +56,94 @@ function adicionar() {
     atualizarQuantidade();
 }
 
-function apagar() { }
+function apagar() {
+    var confirmacao = confirm('Deseja realmente apagar?');
+    if (confirmacao == true) {
+        var elemento = event.target;
+        var elementoTd = elemento.parentNode;
+        var elementoTr = elementoTd.parentNode;
+        var elementoTbody = elementoTr.parentNode;
 
-function preencherCampo() { }
+        var elementoTdNome = elementoTr.childNodes[0];
+        var nome = elementoTdNome.innerHTML;
+        tarefas.pop(nome);
+        atualizarQuantidade();
+        elementoTbody.removeChild(elementoTr);
 
-function editar() { }
+
+        elementoTbody.removeChild(elementoTr);
+    }
+}
+
+function preencherCampo() {
+    var elementoBotaoEditar = event.target;
+    var elementoTr = elementoBotaoEditar.parentNode.parentNode;
+    var elementoTdNome = elementoTr.childNodes[0];
+    var nome = elementoTdNome.innerHTML;
+    indiceParaEditar = tarefas.indexOf(nome);
+    document.getElementById('nome').value = nome;
+    document.getElementById('nome').focus();
+}
+
+function editar() {
+    var nome = document.getElementById('nome').value;
+    tarefas[indiceParaEditar] = nome;
+
+    // atualizar tabela
+var trs = document.getElementById('registros').childNodes;
+var elementoTr = trs[indiceParaEditar];
+elementoTr.childNodes[0].innerHTML = nome;
+
+indiceParaEditar = -1;
+document.getElementById('nome').value = '';
+document.getElementById('nome').focus();
+}
 
 function atualizarQuantidade() {
     document.getElementById("quantidade").innerHTML = tarefas.length;
- }
+}
 
-function limparCampo(campo) { 
+function limparCampo(campo) {
     campo.value = "";
     campo.focus();
 }
 
-function validar(nome, campo) { }
+function validar(nome, campo) {
+    texto = '';
+    if (nome.trim().length == 0) {
+        texto = 'Nome deve ser preenchido';
+    } else if (nome.trim().length < 3) {
+        texto = 'Nome deve conter no mínimo 3 caracteres';
+    } else if (nome.trim().length > 20) {
+        texto = 'Nome deve conter no máximo 20 caracteres';
+    }
+
+    var elementos = document.getElementsByClassName('span-erro');
+
+    for (var i = 0; i < elementos.length; i++) {
+        var elemento = elementos[i];
+        var elementoPai = elemento.parentNode;
+        elementoPai.removeChild(elemento);
+    }
+
+    campo.classList.remove('border-danger', 'text-danger');
+    if (texto != '') {
+        campo.classList.add('border-danger', 'text-danger');
+
+        var spanErro = document.createElement('span');
+        spanErro.innerHTML = texto;
+        spanErro.classList.add('span-erro', 'text-danger', 'font-weight-bold');
+
+
+
+        var elementoPaiDoInput = campo.parentNode;
+        elementoPaiDoInput.appendChild(spanErro);
+
+
+
+        campo.focus();
+        return false;
+    }
+
+    return true;
+}
